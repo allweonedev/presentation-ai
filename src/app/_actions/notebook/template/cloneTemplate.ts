31537ff3f6aa51bd8ecdffdb2e995a88e9adfb75
Prabhat Sitaula <prabhatsitaula@gmail.com>
2026-03-24 22:46:39 +0545

    chore: code cleanup

--- src/app/_actions/notebook/template/cloneTemplate.ts
+++
@@ -1,5 +0,0 @@
-"use server";
-
-export async function cloneTemplate(_templateId: string) {
-  return { error: "Presentation templates are not enabled in this repo yet." };
-}
--- src/lib/getUserNamespace.ts
+++
@@ -1,3 +0,0 @@
-export function getUserNamespace(userId: string) {
-  return `presentation-${userId}`;
-}
